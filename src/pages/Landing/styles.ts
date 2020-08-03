ed5ccaeec4d31fe3ebb2abded1fbc291246d8f5c
Daniel Meireles <dmeireles@pop-os.localdomain>
2020-08-03 20:50:46 -0300

    Landing Page styling (Mobile First)

---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+
+export const PageLanding = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: var(--color-text-in-primary);
+  background: var(--color-primary);
+`;
+
+export const LogoContainer = styled.div`
+  text-align: center;
+  margin-bottom: 3.2rem;
+
+  img {
+    height: 10rem;
+  }
+
+  h2 {
+    font-weight: 500;
+    font-size: 2.4rem;
+    line-height: 4.6rem;
+    margin-top: 0.8rem;
+  }
+`;
+
+export const HeroImage = styled.img`
+  width: 100%;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 3.2rem 0;
+
+  a {
+    width: 30rem;
+    height: 10.4rem;
+    border-radius: 0.8rem;
+    font: 700 2rem Archivo;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    text-decoration: none;
+    color: var(--color-button-text);
+
+    transition: background-color 0.2s;
+
+    img {
+      width: 4rem;
+    }
+  }
+
+  a:first-child {
+    margin-right: 1.6rem;
+  }
+
+  a.study {
+    background: var(--color-primary-lighter);
+  }
+
+  a.give-classes {
+    background: var(--color-secundary);
+  }
+
+  a.study:hover {
+    background: var(--color-primary-light);
+  }
+
+  a.give-classes:hover {
+    background: var(--color-secundary-dark);
+  }
+`;
+
+export const TotalConnections = styled.span`
+  font-size: 1.4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    margin-left: 0.8rem;
+  }
+`;
