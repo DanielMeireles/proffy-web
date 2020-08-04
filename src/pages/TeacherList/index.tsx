9b6b0263f4ba15ade4677c45568b550578e51b92
Daniel Meireles <daniel.meireles@outlook.com>
2020-08-04 01:51:19 -0300

    Add Teacher Item and styling (Making it responsive)

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-import { PageTeacherList, SearchTeachers, Main, TeacherItem } from './styles';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import { PageTeacherList, SearchTeachers, Main } from './styles';
+import TeacherItem from '../../components/TeacherItem';
 
 function TeacherList() {
   return (
@@ -32,34 +31,7 @@ function TeacherList() {
           </SearchTeachers>
         </PageHeader>
         <Main>
-          <TeacherItem>
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/20533324?s=400&u=c2063eb8cd0bf52089b1af605aa10f813b33f67d&v=4"
-                alt="Daniel Meireles"
-              />
-              <div>
-                <strong>Daniel Meireles</strong>
-                <span>Química</span>
-              </div>
-            </header>
-            <p>
-              Teste
-              <br />
-              <br />
-              Teste
-            </p>
-            <footer>
-              <p>
-                Preço/hora
-                <strong>$ 20,00</strong>
-              </p>
-              <button type="button">
-                <img src={whatsappIcon} alt="Whatsapp" />
-                Entrar em contato
-              </button>
-            </footer>
-          </TeacherItem>
+          <TeacherItem />
         </Main>
       </div>
     </PageTeacherList>
